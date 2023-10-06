d1af430ca429610a2fc1e806deaa4bd00466e3ca
4rutemu <artyom.denega@gmail.com>
2023-10-06 13:12:05 +0300

    Задание 1

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,35 @@
 import {encoded, translations} from './data.js'
 
+function decodeArrayOfObjects(encoded, translations) {
+    const decoded = [];
+
+    function decodeObject(obj) {
+        const keysToSkip = ['groupId', 'service', 'formatSize', 'ca'];
+        const decodedObject = {};
+        for (let [key, value] of Object.entries(obj)){
+            if (keysToSkip.includes(key)){
+                decodedObject[key] = value;
+                continue;
+            }
+            if (obj[key]){
+                if (typeof translations[value] !== 'undefined')
+                    decodedObject[key] = translations[value];
+                else
+                    decodedObject[key] = null;
+            }
+        }
+
+        return decodedObject;
+    }
+
+    for (let obj of encoded) {
+        decoded.push(decodeObject(obj));
+    }
+
+    return decoded;
+}
+
 console.log("Let's rock")
 console.log(encoded, translations)
 
-
-
-// console.log(decoded)
+console.log(decodeArrayOfObjects(encoded, translations))
